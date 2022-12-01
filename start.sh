baf2e2561a11d2742f22826703703ae4a9231e90
Зажигин Богдан Алексеевич <za.boal@ya.ru>
2022-12-02 01:37:47 +0300

    Отредактировал команды

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # Сборка и запуск контейнера Docker
-docker build src -t zaboal/bot-telegram_employment;
-docker container run -v /home/zaboal/work/organizations/zazhigin-s_it-studio/databases/local_organizational-structure.db:/usr/src/app/dbase.db zaboal/bot-telegram_employment
+docker build src -t bot-telegram_zaboal-employment;
+docker run bot-telegram_zaboal-employment;
