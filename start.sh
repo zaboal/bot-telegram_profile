15b40dfc3bf2f81e54f988a9a791172e0f94e6a5
Зажигин Богдан Алексеевич <za.boal@ya.ru>
2022-12-05 22:41:05 +0300

    чуть красоты

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
-source .env;
-echo -e "Полученные переменные окружения:\n\t\033[1mтокен Telegram Bot API\033[0m — \033[4m$BOT_TOKEN\033[0m,\n\t\033[1mпуть к базе данных SQLite на хосте\033[0m — \033[4m$HOST_SQLITE_DB_PATH\033[0m.\n\n";
+# Получение необходимых переменных окружения и вывод их в консоль
+source ./env;
+
+echo -e                                                                                 \
+    "Полученные переменные окружения:\n"                                                \
+    "\t\033[1mтокен Telegram Bot API\033[0m — \033[4m$BOT_TOKEN\033[0m,\n"              \
+    "\t\033[1mпуть к базе данных SQLite на хосте\033[0m — \033[4m$DB_PATH\033[0m.\n\n";
+
 
 # Сборка и запуск контейнера Docker
-docker build src -t bot-telegram_zaboal-employment;
+docker build src                                        \
+    --tag bot-telegram_zaboal-employment:latest;
+
 docker run                                              \
     --env BOT_TOKEN=$BOT_TOKEN                          \
-    --volume $DB_PATH:/usr/src/app/dbase.db \
-    bot-telegram_zaboal-employment;
+    --volume $DB_PATH:/usr/src/app/dbase.db             \
+    bot-telegram_zaboal-employment:latest;
