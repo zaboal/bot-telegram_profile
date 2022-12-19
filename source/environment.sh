4c5189a2965b85f38be576e10943f7268709abb7
Зажигин Богдан Алексеевич <za.boal@vk.com>
2022-12-19 19:56:11 +0300

    упростил структуру документации

--- source/environment.sh
+++ source/environment.sh
@@ -2,7 +2,7 @@
 # Настройка проекта подробно описана в docs/settings.md.
 
 # Токен Telegram Bot API получаемый от BotFather
-BOT_TOKEN="5715517585:AAFgAdmzsDokDcCEfy6hO_cI7nrsVeMTx8M"
+BOT_TOKEN="5735526868:AAEyuzE_QNSx5J_VgVcPYxbgK9S9VHCyrlc"
 
 # Путь к базе данных SQLite бота на хосте
 BOT_DB_PATH="/home/zaboal/work/organizations/zaboal-org/databases/telegram-users.sqlite"
